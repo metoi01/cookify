36bf588a30e39be4ef463cce0da0bc5b9be85925
Burakhan Kaldırım <burakhankaldirim@gmail.com>
2023-12-17 02:31:16 +0300

    tasarlanmış sayfalar arası geçiş eklendi.

--- src/main/java/com/example/yenia/HelloController.java
+++ src/main/java/com/example/yenia/HelloController.java
@@ -1,14 +1,53 @@
 package com.example.yenia;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.Pane;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button usernameButton,recipesButton,chiefsButton,myfridgeButton;
+    @FXML
+    private SplitPane recipesPane,usernamePane,chiefsPane,myfridgePane;
+
+    private int currentPage=3;
+    @FXML
+    protected void usernameButtonClick()
+    {
+        changePage(0);
     }
+    @FXML
+    protected void recipesButtonClick()
+    {
+        changePage(3);
+    }
+    @FXML
+    protected void chiefsButtonClick()
+    {
+        changePage(2);
+    }
+    @FXML
+    protected void myfridgeButtonClick()
+    {
+        changePage(4);
+    }
+    public void changePage(int i)
+    {
+        getCurrentPage().setVisible(false);
+        getCurrentPage().setDisable(true);
+        currentPage=i;
+        getCurrentPage().setVisible(true);
+        getCurrentPage().setDisable(false);
+    }
+    public SplitPane getCurrentPage()
+    {
+        if(currentPage==4)return myfridgePane;
+        if(currentPage==3)return recipesPane;
+        if(currentPage==2)return chiefsPane;
+        return usernamePane;
+    }
+
 }
